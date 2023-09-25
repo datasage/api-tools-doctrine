9066a8574b188439b3a596691f248e802b3dcd25
Dan Johansson <datasage@gmail.com>
2023-09-25 15:56:33 -0500

    Added missing files.
    Signed-off-by: Dan Johansson <datasage@gmail.com>

---
+++ src/Server/Service/DoctrineHydrator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Laminas\ApiTools\Doctrine\Server\Service;
+
+use Laminas\Hydrator\HydratorInterface;
+
+/**
+ * Class DoctrineHydrator.
+ */
+class DoctrineHydrator implements HydratorInterface
+{
+    /**
+     * @var HydratorInterface
+     */
+    protected $extractService;
+
+    /**
+     * @var HydratorInterface|\Doctrine\ODM\MongoDB\Hydrator\HydratorInterface
+     */
+    protected $hydrateService;
+
+    /**
+     * @param $extractService
+     * @param $hydrateService
+     */
+    public function __construct($extractService, $hydrateService)
+    {
+        $this->extractService = $extractService;
+        $this->hydrateService = $hydrateService;
+    }
+
+    /**
+     * @return HydratorInterface
+     */
+    public function getExtractService()
+    {
+        return $this->extractService;
+    }
+
+    /**
+     * @return HydratorInterface
+     */
+    public function getHydrateService()
+    {
+        return $this->hydrateService;
+    }
+
+    /**
+     * Extract values from an object.
+     *
+     * @param object $object
+     *
+     * @return array
+     */
+    public function extract(object $object): array
+    {
+        return $this->extractService->extract($object);
+    }
+
+    /**
+     * Hydrate $object with the provided $data.
+     *
+     * @param array $data
+     * @param object $object
+     *
+     * @return object
+     */
+    public function hydrate(array $data, object $object)
+    {
+        // Laminas hydrator:
+        if ($this->hydrateService instanceof HydratorInterface) {
+            return $this->hydrateService->hydrate($data, $object);
+        }
+
+        // Doctrine hydrator: (parameters switched)
+        return $this->hydrateService->hydrate($object, $data);
+    }
+}
